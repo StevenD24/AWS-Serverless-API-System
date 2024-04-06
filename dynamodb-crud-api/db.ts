cb2618f4e53468555073ab27be654dd8055dd557
Steven Duong <stevenduongx@gmail.com>
2024-04-06 00:10:55 -0600

    refactor: db.ts to create a local server

--- dynamodb-crud-api/db.ts
+++ dynamodb-crud-api/db.ts
@@ -1,5 +1,13 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
-const client = new DynamoDBClient({});
+
+const client = new DynamoDBClient({
+    region: "us-west-1",
+    credentials: {
+        accessKeyId: "access_key_id",
+        secretAccessKey: "secret_access_key_id",
+    },
+    endpoint: "http://localhost:8080"
+});
 
 export default client;
