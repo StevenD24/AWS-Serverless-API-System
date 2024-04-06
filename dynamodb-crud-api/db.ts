61fc3952df7d51bc148ea117c44890ea7fcf7c82
Steven Duong <stevenduongx@gmail.com>
2024-04-06 16:34:45 -0600

    add env variable for offline server config

--- dynamodb-crud-api/db.ts
+++ dynamodb-crud-api/db.ts
@@ -1,13 +1,18 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb"; // aws-sdk V3
 
+// Determine if running offline based on the IS_OFFLINE environment variable
+const isOffline = process.env.IS_OFFLINE === 'true';
 
-const dbclient = new DynamoDBClient({
-    region: "us-west-1",
-    credentials: {
-        accessKeyId: "MockAccessKeyId",
-        secretAccessKey: "MockSecretAccessKey",
-    },
-    endpoint: "http://localhost:8000"
-});
+// config for cloud and offline servers
+const config = isOffline ? {
+        region: "us-west-1",
+        credentials: {
+            accessKeyId: "MockAccessKeyId",
+            secretAccessKey: "MockSecretAccessKey",
+        },
+        endpoint: "http://localhost:8000",
+    } : {};
+
+const dbclient = new DynamoDBClient(config);
 
 export default dbclient;
